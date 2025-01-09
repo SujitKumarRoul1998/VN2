ccb4ad972734ccf1c0eb9875db87c7531d91c193
SujitKumarRoul1998 <sujitkumarroul98@gmail.com>
2025-01-09 12:16:15 +0530

    Add VN2 in git hub

---
+++ 1_Spring_Core_01/src/main/java/com/vn2/beans/Vehicle.java
@@ -0,0 +1,29 @@
+package com.vn2.beans;
+
+public class Vehicle {
+
+	private String name;
+
+	public Vehicle() {
+		super();
+	}
+
+	public Vehicle(String name) {
+		super();
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Vehicle [name=" + name + "]";
+	}
+
+}
